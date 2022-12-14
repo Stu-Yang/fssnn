94399ac0e2e9ec4c459a89d32625e59cec98eaf4
stu-yang <1265680871@qq.com>
2022-12-14 09:25:58 +0800

    add some files in baseline-ariann/fcnn

---
+++ src/baseline-ariann/fcnn/fcnn-ariann-3_threading.py
@@ -0,0 +1,124 @@
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+import syft as sy
+
+# Remove compression to have faster communication, because compression time 
+# is non-negligible: we send to workers crypto material which is very heavy
+# and pseudo-random, so compressing it takes a long time and isn't useful:
+# randomness can't be compressed, otherwise it wouldn't be random!
+from syft.serde.compression import NO_COMPRESSION
+sy.serde.compression.default_compress_scheme = NO_COMPRESSION
+
+from data import get_private_data_loaders
+from procedure_copy import train, test          # procedure_copy instead of procedure
+
+# 定义一些常数
+n_train_items = 59904
+n_test_items = 9984
+
+# 定义参与方Alice（P0）和Bob（P1），以及可信第三方crypto_provider
+hook = sy.TorchHook(torch) 
+bob = sy.VirtualWorker(hook, id="bob")
+alice = sy.VirtualWorker(hook, id="alice")
+crypto_provider = sy.VirtualWorker(hook, id="crypto_provider")
+
+workers = [alice, bob]
+sy.local_worker.clients = workers
+
+
+# 定义参数类
+class Arguments():
+    def __init__(self):
+        self.batch_size = 128       # 训练时小批量大小
+        self.test_batch_size = 32   # 验证时小批量大小
+
+        self.n_train_items = n_train_items      # 调整训练数据条目数量
+        self.n_test_items = n_test_items        # 调整测试数据条目数量
+
+        self.epochs = 15            # 训练epoch大小
+        self.lr = 0.01              # 学习率
+        self.seed = 1
+        self.momentum = 0.9
+        self.log_interval = 1      # 每个epoch的日志信息
+        self.precision_fractional = 5   # 小数部分的精度
+        self.requires_grad = True       # requires_grad是Pytorch中通用数据结构Tensor的一个属性，用于说明当前量是否需要在计算中保留对应的梯度信息
+        self.protocol = "fss"
+        self.dtype = "long"
+
+# 定义神经网络：使用3层全连接神经网络
+class FCNN(nn.Module):
+    def __init__(self):
+        super(FCNN, self).__init__()
+        self.fc1 = nn.Linear(784, 128)     # 784 == 28*28
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 10)      # MNIST数据集的分类0～9，共10个类别
+    
+    def forward(self, x):                # 前向传播
+        x = x.reshape(-1, 784)
+        x = F.relu(self.fc1(x))          # 此处的relu函数是秘密协议中的relu函数，不能自定义
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        return x
+
+# 模型训练，并进行验证
+if __name__ == "__main__":
+
+    # 创建和定义参数
+    args = Arguments()
+    torch.manual_seed(args.seed)        # 为CPU设置种子用于生成随机数
+
+    encryption_kwargs = dict(      # 创建加密关键字参数
+        workers=workers, crypto_provider=crypto_provider, protocol=args.protocol    # 在这里调用了fss
+    )
+    kwargs = dict(                  # 创建普通关键字参数
+        requires_grad=args.requires_grad,   # requires_grad是Pytorch中通用数据结构Tensor的一个属性，用于说明当前量是否需要在计算中保留对应的梯度信息
+        precision_fractional=args.precision_fractional,
+        dtype=args.dtype,
+        **encryption_kwargs,        # kwargs包含上述定义的加密关键字参数
+    )
+
+    # 打印模型训练信息
+    print("================================================")
+    print(f"(AriaNN) Ciphertext Training over {args.epochs} epochs")
+    print("model:\t\t", "Fully Connected Neural Network")
+    print("dataset:\t", "MNIST")
+    print("batch_size:\t", args.batch_size)
+    print("================================================")
+
+    # 获得密文状态下训练数据和测试数据
+    private_train_loader, private_test_loader = get_private_data_loaders(args, kwargs)
+
+    # 模型训练
+    model = FCNN()
+    model.encrypt(**kwargs)     # Overloads fix_precision for torch.nn.Module.
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    # optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
+    optimizer = optimizer.fix_precision(
+                    precision_fractional=args.precision_fractional, dtype=args.dtype
+                )
+
+    training_times = []
+    training_comm = []
+
+    for epoch in range(1, args.epochs + 1):
+        
+        (training_times_epoch, training_comm_epoch) = train(args, model, private_train_loader, optimizer, epoch)
+
+        training_times.append(training_times_epoch)
+        training_comm.append(training_comm_epoch)
+
+        test(args, model, private_test_loader)
+
+    
+    print("================================================")
+    print("Online training time: {:.5f}s, and online training comm. is {:.5f}MB"
+         .format(torch.tensor(training_times).mean().item(),
+                 torch.tensor(training_comm ).mean().item()))
+    print("================================================")
+
+    # 模型测试
+    test(args, model, private_test_loader)
---
+++ src/baseline-ariann/fcnn/procedure_copy.py
@@ -0,0 +1,134 @@
+import time
+import torch
+import syft as sy
+import torch.multiprocessing as multiprocessing
+
+# 自定义均方误差函数
+def mse_loss_mean(output, target):
+    batch_size = output.shape[0]
+    loss = ((output - target) ** 2).sum().refresh() / batch_size    # Refresh shares by adding shares of zero
+    return loss
+
+# 定义训练过程
+def train(args, model, private_train_loader, optimizer, epoch):   # 训练函数
+    model.train()
+    times = []      # 记录训练过程的时间
+    comm = []       # 记录训练过程的通信量
+
+    try:
+        n_items = (len(private_train_loader) - 1) * args.batch_size + len(
+            private_train_loader[-1][1]
+        )
+    except TypeError:
+        n_items = len(private_train_loader.dataset)
+
+    for batch_idx, (data, target) in enumerate(private_train_loader):    # 小批量训练
+        start_time = time.time()
+        sy.comm_total = 0
+
+        def forward(optimizer, model, data, target):    # 前向传播，其定义了每次执行的计算步骤，最后返回loss函数
+            optimizer.zero_grad()    # 清空所有被优化过的Variable的梯度
+
+            output = model(data)
+            
+            loss_enc = mse_loss_mean(output, target)    # 计算误差
+            
+            return loss_enc
+
+        loss = [10e10]                     # 初始化loss函数
+        loss_dec = torch.tensor([10e10])
+
+        while loss_dec.abs() > 10:
+            loss[0] = forward(optimizer, model, data, target)   # 前向传播
+
+            loss_dec = loss[0].copy()
+            if loss_dec.is_wrapper:
+                loss_dec = loss_dec.get().float_precision()
+
+            if loss_dec.abs() > 10:
+                print(f'⚠️ #{batch_idx} loss:{loss_dec.item()} RETRY...')
+
+        loss[0].backward()    # 反向传播
+
+        optimizer.step()      # 进行单次优化 (参数更新).
+
+        tot_time = time.time() - start_time
+        tot_comm = sy.comm_total / (2 ** 20)
+
+        times.append(tot_time)
+        comm.append(tot_comm)
+        del sy.comm_total
+
+        if batch_idx % (args.log_interval*10) == 0:
+            print(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tTime: {:.3f}s ({:.3f}s/item)\tComm.:{:.3f}MB [{:.3f}]".format(
+                    epoch,
+                    batch_idx * args.batch_size,
+                    n_items,
+                    100.0 * batch_idx / len(private_train_loader),
+                    loss_dec.item(),
+                    tot_time,
+                    tot_time / args.batch_size,
+                    tot_comm,
+                    args.batch_size,
+                )
+            )
+
+    print()
+    return (torch.tensor(times).sum().item(), torch.tensor(comm).sum().item())
+
+# 模型测试过程
+def test(args, model, private_test_loader):    # 测试函数
+    model.eval()
+    correct = 0
+    times = 0
+    real_times = 0  # with the argmax
+    i = 0
+    try:
+        n_items = (len(private_test_loader) - 1) * args.test_batch_size + len(
+            private_test_loader[-1][1]
+        )
+    except TypeError:
+        n_items = len(private_test_loader.dataset)
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(private_test_loader):
+            i += 1
+            start_time = time.time()
+
+            output = model(data)
+
+            times += time.time() - start_time
+            pred = output.argmax(dim=1)
+            real_times += time.time() - start_time
+            te=pred.eq(target.view_as(pred))
+            p1=pred.copy().get()
+            p2=target.copy().get()
+            ta=p1.eq(p2)
+            #print("pred\n real\n diff{}".format(ta.sum()))
+            correct += ta.sum()
+            if batch_idx % (args.log_interval * 8) == 0 and correct.is_wrapper:
+                c = correct.copy().float_precision()
+                ni = i * args.test_batch_size
+                print(
+                    "Accuracy: {}/{} ({:.0f}%) \tTime / item: {:.4f}s".format(
+                        int(c.item()),
+                        ni,
+                        100.0 * c.item() / ni,
+                        times / ni,
+                    )
+                )
+    if correct.is_wrapper:
+        correct = correct.float_precision()
+
+    print(
+        "TEST Accuracy: {}/{} ({:.2f}%) \tTime /item: {:.4f}s \tTime w. argmax /item: {:.4f}s [{:.3f}]\n".format(
+            correct.item(),
+            n_items,
+            100.0 * correct.item() / n_items,
+            times / n_items,
+            real_times / n_items,
+            args.test_batch_size,
+        )
+    )
+
+    return times, round(100.0 * correct.item() / n_items, 1)
